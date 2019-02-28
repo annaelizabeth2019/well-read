dc3946fa83afa4655039f39bb2741e465a73a28b
Anna Peterson <annapeterson89@gmail.com>
2019-02-27 22:01:35 -0600

    updates the search render page so it is functional, awesome function for determining if an author exists and solves breaking on undefined data types. Solves breaking on undefined thumbnails.

--- controllers/top-sellers.js
+++ controllers/top-sellers.js
@@ -1,4 +1,5 @@
 var key = process.env.GOOGLE_API_KEY;
+var request = require('request');
 const rootUrl = 'https://www.googleapis.com/books/v1/volumes?q=';
 
 module.exports = {
@@ -16,7 +17,7 @@ function index(req, res, next) {
 
 function search(req, res, next) {
     const searchTerms = req.body.search;
-    let searchQuery = searchTerms.toLocaleLowerCase().split(' ').join('+');
+    let searchQuery = searchTerms.toLowerCase().split(' ').join('+');
     var options = {
         url: rootUrl + searchQuery,
         headers: {
@@ -24,7 +25,23 @@ function search(req, res, next) {
         }
       };
       request(options, function(err, response, body){
-          res.render('/top-sellers/search', {bookData: body});
-      });
-    console.log('search:', options)
+        var bookData = JSON.parse(body);
+        // let authorsData;
+        // let title, image;
+        //   for (let i = 0; i < bookData.items.length; i++){
+        //     image = bookData.items[i].volumeInfo.imageLinks.thumbnail;
+        //     authorsData = bookData.items[i].volumeInfo.authors;
+        // }
+        console.log(bookData.items[1])
+        res.render('library/search', {books: bookData.items, user: req.user, authorParser: authorParser, imgParser: imgParser})
+    })
 };
+
+function authorParser(book) {
+    return book === undefined ? 'Author Unknown' : 
+    book.length === 1 ? book.toString() : book.join(', ');
+  };
+
+  function imgParser(data) {
+    return data === undefined ? 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjkl-r1xN3gAhUM26wKHdK5DMMQjRx6BAgBEAU&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3ANo_image_available.svg&psig=AOvVaw2HovIDuMeG12BbSaS62hTI&ust=1551412724166613' : data;
+  };
